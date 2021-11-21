ae989a8b15fb9cf090bec10e372527c6f8d9ddc8
Luiz Lima <luizlima86@gmail.com>
2021-11-21 17:05:19 -0300

    Atualização

--- Teste.R
+++ Teste.R
@@ -1,1 +1,24 @@
-#teste script
+#intala uma biblioteca
+install.packages('brazilmaps', dependencies = T) 
+#dependencies = T ele instala todas as dependencias necessárias para esta biblioteca
+
+#retorna o `help` da biblioteca
+help(package='jsonlite') 
+
+#para usar o pacote
+library(brazilmaps)
+
+#Por exemplo, para a função plot_brmap
+example(plot_brmap)
+
+#sabor algo sobre o comando
+help(nome_do_comando)
+
+#buscar uma função, exemplo, um gráfico de barras
+help.search('bar plot')
+
+#aprender algo sobre o funcionamento
+example(barplot)
+
+hist(rnorm(50))
+
